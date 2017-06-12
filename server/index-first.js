ad0c3ef45a09d22af0261094937bf5933be622e6
Steven Dall <stvndall@gmail.com>
2017-06-12 20:54:15 +0200

    second image

--- server/index-first.js
+++ server/index-first.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('This is the second image')
+    res.send('This is the first image')
 });
 
 app.listen(3000, function() {
