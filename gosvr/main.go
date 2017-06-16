a56a02fd9932d09e7f74fd527c36242a404466b2
Steven Dall <stvndall@gmail.com>
2017-06-16 11:51:11 +0200

    gorilla mux implementation

--- gosvr/main.go
+++ gosvr/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"net/http"
-	"log"
-	"io"
-	"fmt"
 	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func main() {
+	port := flag.Int("port", 8080, "the port number you wish to serve on")
+	flag.Parse()
 
-
-func main(){
-
-port := flag.Int("port", 8080, "the port number you wish to serve on")
-flag.Parse()
-
-http.HandleFunc("/", getRootDetails)
-fmt.Printf("Serving on port %v", *port)
-log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v",*port), nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", getRootDetailsAgain)
+	http.Handle("/", r)
+	fmt.Printf("Serving on port %v", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", *port), nil))
 }
 
-func getRootDetails (w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "this is the first go image")
+func getRootDetailsAgain(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "take details from string")
 }
